2a4532588b09c06714a50ff0b6e9f57316130516
pandabearcoding <madelinerose16@outlook.com>
2020-12-02 00:25:56 -0500

    12/2 early morning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_150857) do
+ActiveRecord::Schema.define(version: 2020_12_02_043749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_150857) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "age"
     t.integer "distance"
+    t.string "url"
   end
 
   create_table "environment_preferences", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,16 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 User.destroy_all
-Dog.destroy_all
+# Dog.destroy_all
 
 ##JOIN
-Match.destroy_all
+# Match.destroy_all
 
 user1 = User.create(username: "mrs161", password: "abc123", name: "Madeline Rose Stalter", profile_picture: "https://cdn.mainichi.jp/vol1/2020/08/31/20200831p2g00m0fe122000p/8.jpg?1", postcode: "11222", age: 25, phone_number: "315-663-7462", email: "MadelineRose16@outlook.com", description: "I exercise daily and would love to have a running buddy! I live near multiple parks in Brookyln (Transmitter, Domino, and Prospect to name a few) and regularly commute to Central Park. I love to cuddle and want a dog that also likes that.", housing_type: "Apartment", has_yard: "No", near_park: "Yes", distance: 100)
 
-dog1 = Dog.create(name: "Sidney", profile_picture: "https://blackthornkennel.com/images/puppies/Aa-Litter/IMG_7144.jpg", age: 2, postcode: "11222", description: "The sweetest girl you'll ever meet! She is happiest when she is snuggled up in your arms. She also LOVES to play fetch, but knows how to settle down while in the house. She is good with cats, dogs, and children.", organization: "Bidawea Animal Rescue", breed: "German Shepherd", size: "large", gender: "female", good_with_cats: true, good_with_dogs: true, good_with_children: true, house_trained: true, spayed_neutered: true, special_needs: false, distance: 1)
+# dog1 = Dog.create(name: "Sidney", profile_picture: "https://blackthornkennel.com/images/puppies/Aa-Litter/IMG_7144.jpg", age: 2, postcode: "11222", description: "The sweetest girl you'll ever meet! She is happiest when she is snuggled up in your arms. She also LOVES to play fetch, but knows how to settle down while in the house. She is good with cats, dogs, and children.", organization: "Bidawea Animal Rescue", breed: "German Shepherd", size: "large", gender: "female", good_with_cats: true, good_with_dogs: true, good_with_children: true, house_trained: true, spayed_neutered: true, special_needs: false, distance: 1)
 
-match1 = Match.create(user_id: user1.id, dog_id: dog1.id)
+# match1 = Match.create(user_id: user1.id, dog_id: dog1.id)
 
 # breed1 = Breed.create(primary: "German Shepherd")
 # breed2 = Breed.create(primary: "Treeing Tennessee Brindle")
